30e45c0ad5c7255406bfcfdf2c38c9af117974b4
Suado Cowboy <luccacsg@gmail.com>
2024-05-19 13:06:52 -0300

    gud.

--- examples/ResizeExample2D.cpp
+++ examples/ResizeExample2D.cpp
@@ -1,67 +1,55 @@
-#include <raylib.h>
+#include "raylib.h"
 
-int main()
+int main(void)
 {
-    const unsigned short baseScreenWidth = 800;
-    const unsigned short baseScreenHeight = 600;
-
-    float SWU = 1.0f; // Screen Width unit
-    float SHU = 1.0f; // Screen Height unit
+    // Initialization
+    const int screenWidth = 800;
+    const int screenHeight = 600;
+    const float targetAspectRatio = 4.0f / 3.0f;
 
     SetConfigFlags(FLAG_WINDOW_RESIZABLE);
-    InitWindow((int)baseScreenWidth, (int)baseScreenHeight, "Screen Width/Height Unit System");
-    
-    Vector2 size = { .x = 200.0f, .y = 100.0f };
-    Vector2 position = { -size.x, (float)baseScreenHeight/2 - size.y/2 };
-    Vector2 velocity = { .x = 1.0f, .y = 1.0f };
+    InitWindow(screenWidth, screenHeight, "raylib - aspect ratio");
 
+    SetConfigFlags(FLAG_WINDOW_RESIZABLE);
     SetTargetFPS(60);
 
-    double startTime = GetTime();
-    double timeDifference = 0.0;
     while (!WindowShouldClose())
     {
-        double dt = GetFrameTime() * 10; // try it later
+        // Calculate new viewport dimensions
+        float windowWidth = (float)GetScreenWidth();
+        float windowHeight = (float)GetScreenHeight();
+        float windowAspectRatio = windowWidth / windowHeight;
 
-        // Move the object
-        position.x += velocity.x * dt;// * dt; // Example movement
-        if (position.x > GetScreenWidth()) {
-            position.x = -size.x; // this might break everything
-            timeDifference = GetTime() - startTime;
-            startTime = GetTime();
+        int newWidth = windowWidth;
+        int newHeight = windowHeight;
+        int xOffset = 0;
+        int yOffset = 0;
+
+        if (windowAspectRatio > targetAspectRatio)
+        {
+            newWidth = (int)(windowHeight * targetAspectRatio);
+            xOffset = (int)((windowWidth - newWidth) / 2);
+        }
+        else if (windowAspectRatio < targetAspectRatio)
+        {
+            newHeight = (int)(windowWidth / targetAspectRatio);
+            yOffset = (int)((windowHeight - newHeight) / 2);
         }
 
-        if (IsWindowResized()) {
-            position.x /= SWU;
-            position.y /= SHU;
-            size.x /= SWU;
-            size.y /= SHU;
-            velocity.x /= SWU;
-            velocity.y /= SHU;
-
-            SWU = (float)GetScreenWidth() / baseScreenWidth;
-            SHU = (float)GetScreenHeight() / baseScreenHeight;
-
-            position.x *= SWU;
-            position.y *= SHU;
-            size.x *= SWU;
-            size.y *= SHU;
-            velocity.x *= SWU;
-            velocity.y *= SHU;
-        }
-
-        // Draw
+        // Update the viewport
         BeginDrawing();
-        ClearBackground(RAYWHITE);
+        ClearBackground(BLACK);
+        BeginScissorMode(xOffset, yOffset, newWidth, newHeight);
+        DrawRectangle(xOffset, yOffset, newWidth, newHeight, RAYWHITE);
 
-        // Draw the object in the 2D world
-        DrawRectangle(position.x, position.y, size.x, size.y, RED);
-        DrawText(TextFormat("%.2f", timeDifference), 0, 0, 24, BLACK);
-        DrawText(TextFormat("%.1f", position.x), 0, 24, 24, BLACK);
+        // Draw content
+        DrawRectangle(xOffset + newWidth / 4, yOffset + newHeight / 4, newWidth / 2, newHeight / 2, RED);
 
+        EndScissorMode();
         EndDrawing();
     }
 
+    // De-Initialization
     CloseWindow();
 
     return 0;
