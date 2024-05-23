15cc2ed9240272d40420ff7a8ee8a8c687c187d5
Suado Cowboy <luccacsg@gmail.com>
2024-05-23 11:55:00 -0300

    created text box(still work in progress)

--- src/Interface/Textbox.cpp
+++ src/Interface/Textbox.cpp
@@ -1,1 +1,61 @@
-#include "Textbox.h"
+#include "TextBox.h"
+
+#include "Utils/Utils.h"
+
+Meatball::TextBox::TextBox()
+    : rect((Rectangle){0,0,0,0}), fontSize(1), color(BLACK),
+    textColor(WHITE), scrollBar((Rectangle){0,0,0,0}) {}
+
+Meatball::TextBox::TextBox(Rectangle rect, unsigned char fontSize)
+    : rect(rect), fontSize(fontSize), color(BLACK), textColor(WHITE),
+    scrollBar((Rectangle){rect.x,rect.y+rect.width, 0, rect.height}) {}
+
+void Meatball::TextBox::appendText(const std::string& newText) {
+    if (MeasureText(newText.c_str(), fontSize) < rect.width-scrollBar.barRect.width) {
+        text.push_back(newText);
+        return;
+    }
+
+    auto it = text.end();
+    std::string newTextCopy = newText;
+    size_t columnIdx = newTextCopy.size()-1;
+    while (MeasureText(newTextCopy.c_str(), fontSize) >= rect.width-scrollBar.barRect.width) {
+        newTextCopy = newText.substr(0, columnIdx);
+        columnIdx--;
+    }
+
+    text.push_back(newTextCopy+'\n'+newText.substr(columnIdx));
+}
+
+void Meatball::TextBox::clearText() {
+    text.clear();
+}
+
+const std::list<std::string>& Meatball::TextBox::getText() {
+    return text;
+}
+
+void Meatball::TextBox::setFontSize(unsigned char newFontSize) {
+    fontSize = newFontSize;
+}
+
+const unsigned char& Meatball::TextBox::getFontSize() {
+    return fontSize;
+}
+
+void Meatball::TextBox::draw() {
+    DrawRectangle(rect.x, rect.y, rect.width, rect.height, color);
+
+    size_t lineIdx = 0;
+    for (auto& line : text) {
+        if (lineIdx*fontSize >= rect.height) break;
+        DrawText(line.c_str(), rect.x, rect.y+(lineIdx*fontSize), fontSize, textColor);
+        lineIdx++;
+    }
+    
+    scrollBar.draw();
+}
+
+void Meatball::TextBox::update() {
+    scrollBar.update();
+}
--- src/Interface/Textbox.h
+++ src/Interface/Textbox.h
@@ -1,1 +1,53 @@
 #pragma once
+
+#include <string>
+#include <list>
+
+#include <raylib.h>
+
+#include "ScrollBar.h"
+
+namespace Meatball {
+    /// @brief basically, a rect being used to draw text
+    /// @note it also have a scrollable bar when text is higher than rect.height
+    /// @note if you're looking for text input see TextInputBox instead
+    class TextBox {
+    public:
+        TextBox();
+        TextBox(Rectangle rect, unsigned char fontSize);
+
+        void setFontSize(unsigned char newFontSize);
+        const unsigned char& getFontSize();
+
+        /// @warning it does not put newline on neither start or end of text(you should put it yourself)
+        void appendText(const std::string& text);
+        void clearText();
+
+        const std::list<std::string>& getText();
+
+        void draw();
+        void update();
+
+        Color color, textColor;
+
+        const Rectangle& getRect();
+
+        void setPosition(Vector2 pos);
+        void setSize(Vector2 size);
+
+        // TODO: Events that can be used to drag and resize rect.
+        // See appendText function, there is a good thing that
+        // should be used in the case of resizing the rect.
+        //VoidFunc onClick, onRelease;
+
+        unsigned char scrollWidth;
+    
+    private:
+        std::list<std::string> text;
+        unsigned char fontSize;
+        
+        ScrollBar scrollBar; // it appears when text is higher than rect.height
+
+        Rectangle rect;
+    };
+}
