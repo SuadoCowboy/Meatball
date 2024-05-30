131272e4e78d81e306e8940cef36f8da0127e7b4
Suado Cowboy <luccacsg@gmail.com>
2024-05-30 01:45:11 -0300

    renamed barRect to rect and added setPosition and setSize functions

--- src/Interface/ScrollBar.cpp
+++ src/Interface/ScrollBar.cpp
@@ -4,25 +4,40 @@
 
 unsigned char Meatball::ScrollBar::scrollSpeed = 50;
 
-static inline void updateScrollValue(float& scrollValue, const float& thumbY, const float& thumbHeight, const Rectangle& barRect) {
-    scrollValue = thumbY / thumbHeight;
+static void setupScrollBarColors(Meatball::ScrollBar* scrollBar) {
+    scrollBar->barColor = (Color){15,15,15,255};
+    scrollBar->barHoveredColor = (Color){25,25,25,255};
+
+    scrollBar->thumbColor = (Color){30,30,30,255};
+    scrollBar->thumbHoveredColor1 = (Color){60,60,60,255};
+    scrollBar->thumbHoveredColor2 = (Color){90,90,90,255};
 }
 
-Meatball::ScrollBar::ScrollBar() : barRect({0,0,0,0}), visible(false),
-    barHovered(false), thumbHovered(false), thumbY(0), thumbHeight(barRect.height),
-    dragging(false), dragOffsetY(0) {}
+Meatball::ScrollBar::ScrollBar() : rect({0,0,0,0}), visible(false),
+ barHovered(false), thumbHovered(false), thumbY(0), thumbHeight(rect.height),
+ dragging(false), dragOffsetY(0) {
+    setupScrollBarColors(this);
+}
 
-Meatball::ScrollBar::ScrollBar(Rectangle barRect, bool visible)
-    : barRect(barRect), visible(visible),
+Meatball::ScrollBar::ScrollBar(Rectangle rect, bool visible)
+    : rect(rect), visible(visible),
     barHovered(false), thumbHovered(false), thumbY(0),
-    thumbHeight(barRect.height), dragging(false), dragOffsetY(0) {
-        barColor = (Color){15,15,15,255};
-        barHoveredColor = (Color){25,25,25,255};
-
-        thumbColor = (Color){30,30,30,255};
-        thumbHoveredColor1 = (Color){60,60,60,255};
-        thumbHoveredColor2 = (Color){90,90,90,255};
+    thumbHeight(rect.height), dragging(false), dragOffsetY(0) {
+        setupScrollBarColors(this);
     }
+
+void Meatball::ScrollBar::setPosition(float x, float y) {
+    rect.x = x;
+    rect.y = y;
+}
+
+void Meatball::ScrollBar::setSize(float width, float height) {
+    if (rect.height != 0)
+        thumbY *= height/rect.height;
+    
+    rect.width = width;
+    rect.height = height;
+}
 
 float Meatball::ScrollBar::getScrollValue() const {
     return scrollValue;
@@ -31,36 +46,36 @@ float Meatball::ScrollBar::getScrollValue() const {
 void Meatball::ScrollBar::draw() {
     if (!visible) return;
 
-    DrawRectangle(barRect.x, barRect.y, barRect.width, barRect.height, barHovered? barHoveredColor : barColor);
+    DrawRectangle(rect.x, rect.y, rect.width, rect.height, barHovered? barHoveredColor : barColor);
 
     Color* actualScrollColor;
     if (barHovered) actualScrollColor = &thumbHoveredColor1;
     else actualScrollColor = &thumbColor;
     if (thumbHovered) actualScrollColor = &thumbHoveredColor2;
 
-    DrawRectangle(barRect.x, barRect.y+thumbY, barRect.width, thumbHeight, *actualScrollColor);
+    DrawRectangle(rect.x, rect.y+thumbY, rect.width, thumbHeight, *actualScrollColor);
 }
 
 void Meatball::ScrollBar::update(const Rectangle& parentRect) {
     Vector2 mousePosition = GetMousePosition();
     
-    barHovered = CheckCollisionPointRec(mousePosition, barRect);
+    barHovered = CheckCollisionPointRec(mousePosition, rect);
     
     thumbHovered = CheckCollisionPointRec(mousePosition,
-        {barRect.x, barRect.y+thumbY, barRect.width, thumbHeight});
+        {rect.x, rect.y+thumbY, rect.width, thumbHeight});
 
     if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
         if (thumbHovered) {
             dragging = true;
             dragOffsetY = thumbY - mousePosition.y;
         } else if (barHovered)
-            thumbY = mousePosition.y - thumbHeight/2 - barRect.y;
+            thumbY = mousePosition.y - thumbHeight/2 - rect.y;
         
     } else if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT)) dragging = false;
     
     float mouseWheelMove = GetMouseWheelMove();
     if (mouseWheelMove != 0 && CheckCollisionPointRec(GetMousePosition(), parentRect)) {
-        thumbY -= mouseWheelMove * scrollSpeed * (thumbHeight/barRect.height);
+        thumbY -= mouseWheelMove * scrollSpeed * (thumbHeight/rect.height);
     }
     
     if (dragging) {
@@ -70,12 +85,16 @@ void Meatball::ScrollBar::update(const Rectangle& parentRect) {
     // if up limit
     if (thumbY < 0) thumbY = 0;
     // if down limit
-    if (thumbY+thumbHeight > barRect.height) thumbY = barRect.height-thumbHeight;
+    if (thumbY+thumbHeight > rect.height) thumbY = rect.height-thumbHeight;
     
-    updateScrollValue(scrollValue, thumbY, thumbHeight, barRect);
+    scrollValue = thumbY / thumbHeight;
+}
+
+const Rectangle& Meatball::ScrollBar::getRect() {
+    return rect;
 }
 
 void Meatball::ScrollBar::updateThumbHeight(float viewHeight, float contentHeight) {
-    thumbHeight = (viewHeight / contentHeight) * barRect.height;
+    thumbHeight = (viewHeight / contentHeight) * rect.height;
     if (thumbHeight < 1) thumbHeight = 1;
 }
