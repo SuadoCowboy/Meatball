4c61a345d1f8f083fa1c28e6a2b446349d4dd900
Suado Cowboy <luccacsg@gmail.com>
2024-05-31 01:32:50 -0300

    omg i almost let spaghetti code pass

--- src/Interface/InputTextBox.cpp
+++ src/Interface/InputTextBox.cpp
@@ -68,106 +68,90 @@ void Meatball::InputTextBox::update() {
     if (!focused) return;
 
     bool selectMode = IsKeyDown(KEY_LEFT_SHIFT) || IsKeyDown(KEY_RIGHT_SHIFT);
-
-    if (IsKeyDown(KEY_LEFT_CONTROL) || IsKeyDown(KEY_RIGHT_CONTROL)) { // Move
-        if (cursorPos != 0) { // left logic
-            if (IsKeyPressed(KEY_LEFT) || IsKeyPressedRepeat(KEY_LEFT)) {
-                // conditions are: shift key down;
-                // endIdx/beginIdx not set yet OR they are equal(in same position)
-                if (selectMode && selectedTextEndIdx == selectedTextBeginIdx)
+    bool moveMode = IsKeyDown(KEY_LEFT_CONTROL) || IsKeyDown(KEY_RIGHT_CONTROL);
+       
+    // CTRL+A/SHIFT+LEFT/RIGHT
+    /*
+    Possibilities:
+    A = SELECT ALL(todo select logic)
+    */
+    
+    if (IsKeyPressed(KEY_LEFT) || IsKeyPressedRepeat(KEY_LEFT)) {
+        if (selectMode && selectedTextEndIdx == selectedTextBeginIdx)
                     selectedTextEndIdx = cursorPos;
 
-                while (cursorPos != 0 && text[cursorPos-1] == ' ') --cursorPos; // skip whitespaces to go to word if there's any
-                while (cursorPos != 0 && text[cursorPos-1] != ' ') --cursorPos; // skip the word until end of the word
-                
-                if (selectMode) { // aaa bbb
-                    if (selectedTextBeginIdx > cursorPos)
-                        selectedTextBeginIdx = cursorPos;
-                    else selectedTextEndIdx = cursorPos;
-                } else {
-                    selectedTextBeginIdx = std::string::npos;
-                    selectedTextEndIdx = std::string::npos;
-                }
-
-            } else if (IsKeyPressed(KEY_BACKSPACE) || IsKeyPressedRepeat(KEY_BACKSPACE)) {
-                // if already erased space, to not erase other chars
-                bool usedSpaceVersion = false;
-                
-                while (cursorPos != 0 && text[cursorPos-1] == ' ') {
-                    text.erase(text.begin()+cursorPos-1);
-                    --cursorPos;
-                    usedSpaceVersion = true;
-                }
-                
-                if (!usedSpaceVersion) while (cursorPos != 0 && text[cursorPos-1] != ' ') {
-                    text.erase(text.begin()+cursorPos-1);
-                    --cursorPos;
-                }
-                
-                if (onTextChange) onTextChange(text);
-            }
-        }
-        if (cursorPos != text.size()) { // right logic
-            if (IsKeyPressed(KEY_RIGHT) || IsKeyPressedRepeat(KEY_RIGHT)) {
-                // conditions are: shift key down;
-                // endIdx/beginIdx not set yet OR they are equal(in same position)
-                if (selectMode && selectedTextBeginIdx == selectedTextEndIdx)
-                    selectedTextBeginIdx = cursorPos;
-
-                while (cursorPos != text.size() && text[cursorPos] == ' ') ++cursorPos;
-                while (cursorPos != text.size() && text[cursorPos] != ' ') ++cursorPos;
-                
-                if (selectMode) {
-                    if (selectedTextEndIdx < cursorPos || selectedTextEndIdx == std::string::npos)
-                        selectedTextEndIdx = cursorPos;
-                    else selectedTextBeginIdx = cursorPos;
-                } else {
-                    selectedTextBeginIdx = std::string::npos;
-                    selectedTextEndIdx = std::string::npos;
-                }
-            
-            } else if (IsKeyPressed(KEY_DELETE) || IsKeyPressedRepeat(KEY_DELETE)) {
-                // if already erased space, to not erase other chars
-                bool usedSpaceVersion = false;
-                
-                while (cursorPos != text.size() && text[cursorPos] == ' ') {
-                    text.erase(text.begin()+cursorPos);
-                    usedSpaceVersion = true;
-                }
-                
-                if (!usedSpaceVersion) while (cursorPos != text.size() && text[cursorPos] != ' ')
-                    text.erase(text.begin()+cursorPos);
-                
-                if (onTextChange) onTextChange(text);
-            }
-        }
+        if (moveMode) {
+            while (cursorPos != 0 && text[cursorPos-1] == ' ') --cursorPos;
+            while (cursorPos != 0 && text[cursorPos-1] != ' ') --cursorPos;
+        } else if (cursorPos != 0) --cursorPos;
         
-        // CTRL+A/SHIFT+LEFT/RIGHT
-        /*
-        Possibilities:
-        A = SELECT ALL(todo select logic)
-        */
-
-    } else if (IsKeyPressed(KEY_LEFT) || IsKeyPressedRepeat(KEY_LEFT)) {
-        if (cursorPos != 0) --cursorPos;
-        selectedTextBeginIdx = std::string::npos;
-        selectedTextEndIdx = std::string::npos;
+        if (selectMode) {
+            if (selectedTextBeginIdx >= cursorPos)
+                selectedTextBeginIdx = cursorPos;
+            else selectedTextEndIdx = cursorPos;
+        } else {
+            selectedTextBeginIdx = std::string::npos;
+            selectedTextEndIdx = std::string::npos;
+        }
     }
+    
     else if (IsKeyPressed(KEY_RIGHT) || IsKeyPressedRepeat(KEY_RIGHT)) {
-        if (cursorPos != text.size()) ++cursorPos;
-        selectedTextBeginIdx = std::string::npos;
-        selectedTextEndIdx = std::string::npos;
+        if (selectMode && selectedTextBeginIdx == selectedTextEndIdx)
+                    selectedTextBeginIdx = cursorPos;
+        
+        if (moveMode) {
+            while (cursorPos != text.size() && text[cursorPos] == ' ') ++cursorPos;
+            while (cursorPos != text.size() && text[cursorPos] != ' ') ++cursorPos;
+        } else if (cursorPos != text.size()) ++cursorPos;
+        
+        if (selectMode) {
+            if (selectedTextEndIdx <= cursorPos || selectedTextEndIdx == std::string::npos)
+                selectedTextEndIdx = cursorPos;
+            else selectedTextBeginIdx = cursorPos;
+        } else {
+            selectedTextBeginIdx = std::string::npos;
+            selectedTextEndIdx = std::string::npos;
+        }
     }
 
     else if ((IsKeyPressed(KEY_BACKSPACE) || IsKeyPressedRepeat(KEY_BACKSPACE)) && cursorPos != 0) {
-        text.erase(text.begin()+cursorPos-1);
-        --cursorPos;
+        if (moveMode) {
+            // if already erased space, to not erase other chars
+            bool usedSpaceVersion = false;
+            
+            while (cursorPos != 0 && text[cursorPos-1] == ' ') {
+                text.erase(text.begin()+cursorPos-1);
+                --cursorPos;
+                usedSpaceVersion = true;
+            }
+            
+            if (!usedSpaceVersion) while (cursorPos != 0 && text[cursorPos-1] != ' ') {
+                text.erase(text.begin()+cursorPos-1);
+                --cursorPos;
+            }
+        } else {
+            text.erase(text.begin()+cursorPos-1);
+            --cursorPos;
+        }
         
         if (onTextChange) onTextChange(text);
     }
 
     else if ((IsKeyPressed(KEY_DELETE) || IsKeyPressedRepeat(KEY_DELETE)) && cursorPos != text.size()) {
-        text.erase(text.begin()+cursorPos);
+        if (moveMode) {
+            // if already erased space, to not erase other chars
+            bool usedSpaceVersion = false;
+            
+            while (cursorPos != text.size() && text[cursorPos] == ' ') {
+                text.erase(text.begin()+cursorPos);
+                usedSpaceVersion = true;
+            }
+            
+            if (!usedSpaceVersion) while (cursorPos != text.size() && text[cursorPos] != ' ')
+                text.erase(text.begin()+cursorPos);
+        } else
+            text.erase(text.begin()+cursorPos);
+        
         if (onTextChange) onTextChange(text);
     }
     
