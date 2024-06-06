601e7649fe28542c6ca888843dbbe21e91e26511
Suado Cowboy <luccacsg@gmail.com>
2024-06-05 22:34:19 -0300

    made Button class cleaner

--- src/Interface/Button.cpp
+++ src/Interface/Button.cpp
@@ -1,83 +1,15 @@
 #include "Button.h"
 
 #include "Utils/Utils.h"
-#include "FontsHandler.h"
-#include "ColorsHandler.h"
 
-using fh = Meatball::FontsHandler;
-using ch = Meatball::ColorsHandler;
+Meatball::Button::Button(Config::Button &config)
+ : config(config), rect({0,0,0,0}) {}
 
-static inline void setupButton(Meatball::Button *button) {
-    button->onHover = nullptr;
-    button->onRelease = nullptr;
-
-    button->color = ch::get("button");
-    button->hoveredColor = ch::get("buttonHovered");
-
-    button->textColor = WHITE;
-    button->hoveredTextColor = RAYWHITE;
-}
-
-Meatball::Button::Button() : rect{.x=0,.y=0,.width=0,.height=0} {
-    setupButton(this);
-}
-
-Meatball::Button::Button(float x, float y, float width, float height) {
-        rect = {x, y, width, height};
-        setupButton(this);
-    }
-
-Meatball::Button::Button(Rectangle rect) : rect(rect) {
-    setupButton(this);
-}
+Meatball::Button::Button(Rectangle &rect, Config::Button &config)
+ : config(config), rect(rect) {}
 
 void Meatball::Button::update() {
     checkHovered(hovered, rect, &onHover, &onRelease);
-}
-
-const std::string& Meatball::Button::getText() {
-    return text;
-}
-
-void Meatball::Button::setText(std::string newText) {
-    int textWidth = fh::MeasureTextWidth(font, newText.c_str());
-
-    while (textWidth > rect.width) {
-        newText = newText.substr(0, newText.size()-1);
-        textWidth = fh::MeasureTextWidth(font, newText.c_str());
-    }
-
-    text = newText;
-}
-
-void Meatball::Button::drawText()
-{   
-    fh::DrawText(font,text.c_str(), rect.x, rect.y, hovered? hoveredTextColor : textColor);
-}
-
-void Meatball::Button::drawTextCentered(bool centerX, bool centerY) {
-    int textWidthHalf = fh::MeasureTextWidth(font, text.c_str())/2;
-
-    fh::DrawText(font, text.c_str(), centerX? rect.x+rect.width/2-textWidthHalf : rect.x, centerY? rect.y+rect.height/2-font->baseSize/2 : rect.y, hovered? hoveredTextColor : textColor);
-}
-
-void Meatball::Button::drawRect() {
-    DrawRectangle(rect.x, rect.y, rect.width, rect.height, hovered? hoveredColor : color);
-}
-
-void Meatball::Button::drawX() {
-    /*
-    .
-     .
-      .
-    */
-    DrawLine(rect.x,rect.y, rect.x+rect.width,rect.y+rect.height, hovered? hoveredColor : color);
-    /*
-      .
-     .
-    .
-    */
-    DrawLine(rect.x,rect.y+rect.height, rect.x+rect.width,rect.y, hovered? hoveredColor : color);
 }
 
 bool Meatball::Button::isHovered()
--- src/Interface/Button.h
+++ src/Interface/Button.h
@@ -5,45 +5,22 @@
 #include <raylib.h>
 
 #include "Shared.h"
+#include "Config/Interface.h"
 
 namespace Meatball {
-    class Button
-    {
+    class Button {
     public:
-        Button();
-        Button(float x, float y, float width, float height);
-        Button(Rectangle rect);
+        Button(Config::Button &config);
+        Button(Rectangle &rect, Config::Button &config);
         
         void update();
-
-        /// @brief draws text inside the button bounds
-        void drawText();
-
-        /// @brief draws text inside the button bounds but centered
-        /// @param centerX if should center on x axis
-        /// @param centerY if should center on y axis
-        void drawTextCentered(bool centerX, bool centerY);
-        
-        /// @brief draws the button rect only.
-        void drawRect();
-
-        /// @brief draws a X inside the rect bounds
-        void drawX();
-
         bool isHovered();
 
-        const std::string& getText();
-
-        void setText(std::string newText);
-
         VoidFunc onRelease, onHover;
+        Config::Button &config;
         Rectangle rect;
-        Color *color, *hoveredColor, *textColor, *hoveredTextColor; // colors that are equal in other interfaces
-        Font *font;
 
     private:
         bool hovered;
-
-        std::string text;
     };
 }
