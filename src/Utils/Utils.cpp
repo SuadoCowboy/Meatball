db8e68d23e2bc9b495fc58e96054d0f6226dd239
Suado Cowboy <luccacsg@gmail.com>
2024-06-05 22:33:43 -0300

    added getRectCenter function

--- src/Utils/Utils.cpp
+++ src/Utils/Utils.cpp
@@ -1,6 +1,6 @@
 #include "Utils.h"
 
-void Meatball::checkHovered(bool& hovered, const Rectangle& rect, VoidFunc *onHover, VoidFunc *onRelease) {
+void Meatball::checkHovered(bool &hovered, const Rectangle &rect, VoidFunc *onHover, VoidFunc *onRelease) {
     bool wasHovered = hovered;
     hovered = CheckCollisionPointRec(GetMousePosition(), rect);
     
@@ -11,9 +11,9 @@ void Meatball::checkHovered(bool& hovered, const Rectangle& rect, VoidFunc *onHo
         (*onRelease)();
 }
 
-float Meatball::getContentHeight(float fontSize, const std::list<std::string>& text) {
+float Meatball::getContentHeight(float fontSize, const std::list<std::string> &text) {
     size_t lineIdx = 0;
-    for (auto& line : text) {
+    for (auto &line : text) {
         size_t newLineIdx = 0;
         while (newLineIdx != std::string::npos) {
             newLineIdx = line.find('\n', newLineIdx+1);
@@ -24,7 +24,19 @@ float Meatball::getContentHeight(float fontSize, const std::list<std::string>& t
     return lineIdx*fontSize; // contentHeight
 }
 
-bool operator==(const Color& left, const Color& right) {
+Vector2 Meatball::getRectCenter(const Rectangle &rect) {
+    return {rect.x+rect.width*0.5, rect.y+rect.height*0.5};
+}
+
+float Meatball::getRectCenterX(const Rectangle &rect) {
+    return rect.x+rect.width*0.5;
+}
+
+float Meatball::getRectCenterY(const Rectangle &rect) {
+    return rect.y+rect.height*0.5;
+}
+
+bool operator==(const Color &left, const Color &right) {
     return (left.r == right.r) && 
      (left.g == right.g) && 
      (left.b == right.b) && 
--- src/Utils/Utils.h
+++ src/Utils/Utils.h
@@ -9,9 +9,13 @@
 
 namespace Meatball {
     /// @brief since alot of classes might do hover check, this function exists to facilitate the process of implementing it
-    void checkHovered(bool& hovered, const Rectangle& rect, VoidFunc *onHover, VoidFunc *onRelease);
+    void checkHovered(bool &hovered, const Rectangle &rect, VoidFunc *onHover, VoidFunc *onRelease);
 
-    float getContentHeight(float fontSize, const std::list<std::string>& text);
+    float getContentHeight(float fontSize, const std::list<std::string> &text);
+
+    Vector2 getRectCenter(const Rectangle &rect);
+    float getRectCenterX(const Rectangle &rect);
+    float getRectCenterY(const Rectangle &rect);
 }
 
-bool operator==(const Color& left, const Color& right);
+bool operator==(const Color &left, const Color &right);
