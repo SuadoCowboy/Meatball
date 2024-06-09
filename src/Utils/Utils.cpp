331c8cc65899b18e5cdedc9894fc155829c9b180
Suado Cowboy <luccacsg@gmail.com>
2024-06-09 14:04:08 -0300

    yup, not using const char*

--- src/Utils/Utils.cpp
+++ src/Utils/Utils.cpp
@@ -1,7 +1,5 @@
 #include "Utils.h"
 
-#include <cstring>
-
 void Meatball::checkHovered(bool &hovered, const Rectangle &rect, VoidFunc *onHover, VoidFunc *onRelease) {
     bool wasHovered = hovered;
     hovered = CheckCollisionPointRec(GetMousePosition(), rect);
@@ -11,19 +9,6 @@ void Meatball::checkHovered(bool &hovered, const Rectangle &rect, VoidFunc *onHo
     
     if (hovered && onRelease && *onRelease && IsMouseButtonReleased(MOUSE_BUTTON_LEFT))
         (*onRelease)();
-}
-
-float Meatball::getContentHeight(float fontSize, const std::list<std::string> &text) {
-    size_t lineIdx = 0;
-    for (auto &line : text) {
-        size_t newLineIdx = 0;
-        while (newLineIdx != std::string::npos) {
-            newLineIdx = line.find('\n', newLineIdx+1);
-            lineIdx++;
-        }
-    }
-
-    return lineIdx*fontSize; // contentHeight
 }
 
 Vector2 Meatball::getRectCenter(const Rectangle &rect) {
@@ -44,42 +29,6 @@ void Meatball::fitXYInRenderScreen(Rectangle &rect, const Vector2 &minPos, const
     
     if (rect.y < minPos.y) rect.y = minPos.y;
     else if (rect.y > GetRenderHeight()-maxPos.y) rect.y = GetRenderHeight()-maxPos.y;
-}
-
-void Meatball::textErase(char* text, size_t index, size_t length) {
-    if (strlen(text) < index+length || strlen(text) == 0) return;
-    
-    while (true) {
-		text[index] = text[index+length];
-		if (text[index+length] == '\0') break;
-		++index;
-	}
-
-    text[index] = '\0';
-}
-
-size_t Meatball::textInsert(char* dest, size_t destMaxSize, size_t destIdx, const char* source) {
-    size_t sourceIdx = 0;
-    size_t destSize = strlen(dest);
-	
-	while (source[sourceIdx] != '\0' && destSize != destMaxSize) {
-		char charBefore = dest[destIdx];
-		dest[destIdx] = source[sourceIdx];
-		++destSize;
-	
-		for (size_t i = destIdx+1; i < destSize; ++i) {
-			char actualChar = dest[i];
-			dest[i] = charBefore;
-			charBefore = actualChar;
-		}
-
-		++destIdx;
-		++sourceIdx;
-	}
-
-	dest[destSize] = '\0';
-
-    return destIdx;
 }
 
 bool operator==(const Color &left, const Color &right) {
--- src/Utils/Utils.h
+++ src/Utils/Utils.h
@@ -1,8 +1,5 @@
 #pragma once
 
-#include <list>
-#include <string>
-
 #include <raylib.h>
 
 #include "Shared.h"
@@ -10,9 +7,7 @@
 namespace Meatball {
     /// @brief since alot of classes might do hover check, this function exists to facilitate the process of implementing it
     void checkHovered(bool &hovered, const Rectangle &rect, VoidFunc *onHover, VoidFunc *onRelease);
-
-    float getContentHeight(float fontSize, const std::list<std::string> &text);
-
+    
     Vector2 getRectCenter(const Rectangle &rect);
     float getRectCenterX(const Rectangle &rect);
     float getRectCenterY(const Rectangle &rect);
@@ -20,16 +15,6 @@ namespace Meatball {
     /// @brief modifies the rect position to fit in the render screen
     /// @param maxPos the max pos is already counted with render position so you don't need to sum it yourself
     void fitXYInRenderScreen(Rectangle &rect, const Vector2 &minPos, const Vector2 &maxPos);
-
-    /// @brief erases from index until index+length
-    void textErase(char* dest, size_t index, size_t length);
-
-    /// @brief inserts source until dest max size is reached
-    /// @note check if your dest is not at it's max len(at least 1 free space required)
-    /// @note dest can not be empty
-    /// @warning I don't actually know if this is a safe function and probably it's not even efficient
-    /// @return index at end of source in dest
-    size_t textInsert(char* dest, size_t destMaxSize, size_t index, const char* source);
 }
 
 bool operator==(const Color &left, const Color &right);
