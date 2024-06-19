1615ebb113131ff9e5e2f3c8decc02c048bb6206
Suado Cowboy <luccacsg@gmail.com>
2024-06-19 13:46:33 -0300

    guess Input class is finished

--- src/Input.cpp
+++ src/Input.cpp
@@ -6,20 +6,35 @@
 
 #include "Console.h"
 
+std::unordered_map<std::string, Meatball::InputData> Meatball::Input::keyState;
+std::unordered_map<std::string, Meatball::InputData> Meatball::Input::mouseState;
+
+std::string Meatball::Input::mouseWheelUpCallback;
+std::string Meatball::Input::mouseWheelDownCallback;
+
 void Meatball::Input::bind(std::string keyName, const std::string& callback) {
 	std::transform(keyName.begin(), keyName.end(), keyName.begin(),
     [](unsigned char c){ return std::tolower(c); });
 
-	if (keyState.count(keyName) == 0) return;
+	bool isMouseButton = mouseState.count(keyName) != 0;
+	bool isMouseWheelUp = keyName == "mwheelup";
+	bool isMouseWheelDown = keyName == "mwheeldown";
+
+	if (isMouseWheelUp)
+		mouseWheelUpCallback = callback;
+	else if (isMouseWheelDown)
+		mouseWheelDownCallback = callback;
+	else if (keyState.count(keyName) == 0 && !isMouseButton) return;
 	
-	keyState[keyName].callback = callback;
+	if (!isMouseButton) keyState[keyName].callback = callback;
+	else mouseState[keyName].callback = callback;
 	
 	std::stringstream stream;
 	HayBCMD::Lexer lexer{callback};
 	HayBCMD::Token token;
 	while (token.getType() != HayBCMD::TokenType::_EOF) {
 		if (token.getType() == HayBCMD::TokenType::COMMAND && token.getValue()[0] == '+') {
-			stream << token.getValue() << ";";
+			stream << '-'+token.getValue().substr(1) << ";";
 			
 			while (token.getType() != HayBCMD::TokenType::_EOF && token.getType() != HayBCMD::TokenType::EOS)
 				token = lexer.nextToken();
@@ -28,18 +43,92 @@ void Meatball::Input::bind(std::string keyName, const std::string& callback) {
 		token = lexer.nextToken();
 	}
 
-	keyState[keyName].offCallback = stream.str();
-	keyState[keyName].offCallback = keyState[keyName].offCallback.substr(0, keyState[keyName].offCallback.size()-1);
+	std::string offCallback = stream.str();
+	offCallback = offCallback.substr(0, offCallback.size()-1);
+
+	if (offCallback.size() == 0) return;
+
+	if (isMouseWheelUp) // mouse wheel up
+		mouseWheelUpCallback += ';'+offCallback;
+	
+	else if (isMouseWheelDown) // mouse wheel down
+		mouseWheelDownCallback += ';'+offCallback;
+	
+	else if (isMouseButton) // mouse buttons
+		mouseState[keyName].offCallback = offCallback;
+	else // keyboard
+		keyState[keyName].offCallback = offCallback;
 }
 
 void Meatball::Input::unbind(const std::string& keyName) {
-	if (keyState.count(keyName) == 0) return;
+	if (keyState.count(keyName) != 0) {
+		keyState[keyName].callback = "";
+		keyState[keyName].offCallback = "";
+	
+	} else if (mouseState.count(keyName) != 0) {
+		mouseState[keyName].callback = "";
+		mouseState[keyName].offCallback = "";
+	
+	} else if (keyName == "mwheelup")
+		mouseWheelUpCallback = "";
+	
+	else if (keyName == "mwheeldown")
+		mouseWheelDownCallback = "";
+}
 
-	keyState[keyName].callback = "";
+void Meatball::Input::bindCommand(HayBCMD::Command*, const std::vector<std::string>& args) {
+	if (args.size() != 1) { // set
+		bind(args[0], args[1]);
+		return;
+	}
+
+	// get
+	std::string callback;
+	if (keyState.count(args[0]) != 0)
+		callback = keyState[args[0]].callback;
+	
+	else if (mouseState.count(args[0]) != 0)
+		callback = keyState[args[0]].callback;
+	
+	else if (args[0] == "mwheelup")
+		callback = mouseWheelUpCallback;
+	
+	else if (args[0] == "mwheeldown")
+		callback = mouseWheelDownCallback;
+	else {
+		Console::printf(HayBCMD::ERROR, "unknown key {}\n", args[0]);
+		return;
+	}
+
+	Console::printf(HayBCMD::ECHO, "{} = \"{}\"\n", args[0], callback);
+}
+
+void Meatball::Input::unBindCommand(HayBCMD::Command*, const std::vector<std::string>& args) {
+	unbind(args[0]);
+}
+
+void Meatball::Input::unBindAllCommand(HayBCMD::Command*, const std::vector<std::string>& args) {
+	for (auto& key : keyState) {
+		key.second.callback = "";
+		key.second.offCallback = "";
+	}
+
+	for (auto& button : mouseState) {
+		button.second.callback = "";
+		button.second.offCallback = "";
+	}
+
+	mouseWheelUpCallback = mouseWheelDownCallback = "";
+}
+
+void Meatball::Input::registerCommands() {
+	HayBCMD::Command("bind", 1, 2, bindCommand, "<key> <commands?> - binds a key to a command");
+	HayBCMD::Command("unbind", 1, 1, unBindCommand, "<key> - unbinds a key");
+	HayBCMD::Command("unbindall", 0, 0, unBindAllCommand, "unbinds all keys");
 }
 
 void Meatball::Input::setKey(const std::string& name, unsigned short code) {
-	keyState[name] = {code, ""};
+	keyState[name] = {code, "", ""};
 }
 
 void Meatball::Input::removeKey(const std::string& name) {
@@ -47,7 +136,7 @@ void Meatball::Input::removeKey(const std::string& name) {
 }
 
 void Meatball::Input::setMouseKey(const std::string& name, unsigned short code) {
-	mouseState[name] = {code, ""};
+	mouseState[name] = {code, "", ""};
 }
 
 void Meatball::Input::removeMouseKey(const std::string& name) {
@@ -70,4 +159,11 @@ void Meatball::Input::update() {
 		else if (IsKeyReleased(button.second.code)) 
 			Console::run(button.second.offCallback);
 	}
+
+	float wheel = GetMouseWheelMove();
+	if (wheel == 1.0f)
+		Console::run(mouseWheelUpCallback);
+	
+	else if (wheel == -1.0f)
+		Console::run(mouseWheelDownCallback);
 }
--- src/Input.h
+++ src/Input.h
@@ -2,11 +2,14 @@
 
 #include <unordered_map>
 #include <string>
+#include <vector>
 
 #include <raylib.h>
 
+#include <HayBCMD.h>
+
 namespace Meatball {
-	struct KeyData {
+	struct InputData {
 		unsigned short code;
 		std::string callback;
 		std::string offCallback; // to turn off all the toggle types that might be in the callback
@@ -14,28 +17,35 @@ namespace Meatball {
 
 	class Input {
 	public:
-		void bind(std::string keyName, const std::string& callback);
-		void unbind(const std::string& keyName);
+		static void bind(std::string keyName, const std::string& callback);
+		static void unbind(const std::string& keyName);
+
+		static void bindCommand(HayBCMD::Command*, const std::vector<std::string>& args);
+		static void unBindCommand(HayBCMD::Command*, const std::vector<std::string>& args);
+		static void unBindAllCommand(HayBCMD::Command*, const std::vector<std::string>& args);
 		
+		static void registerCommands();
+
 		/// @brief registers a key to be checked if is pressed or not
 		/// @param name the name of the key ex: lalt
 		/// @param code the raylib code of the key ex: KEY_LEFT_ALT
-		void setKey(const std::string& name, unsigned short code);
+		static void setKey(const std::string& name, unsigned short code);
 		
-		void removeKey(const std::string& name);
+		static void removeKey(const std::string& name);
 
 		/// @brief registers a key to be checked if is pressed or not
 		/// @param name the name of the key ex: mouse1
 		/// @param code the raylib code of the key ex: MOUSE_BUTTON_LEFT
-		void setMouseKey(const std::string& name, unsigned short code);
-		void removeMouseKey(const std::string& name);
+		static void setMouseKey(const std::string& name, unsigned short code);
+		static void removeMouseKey(const std::string& name);
 
-		void update();
+		static void update();
 
 	private:
-		static std::unordered_map<std::string, KeyData> keyState;
-		static std::unordered_map<std::string, KeyData> mouseState;
-		static KeyData mouseWheelUp;
-		static KeyData mouseWheelDown;
+		static std::unordered_map<std::string, InputData> keyState;
+		static std::unordered_map<std::string, InputData> mouseState;
+		
+		static std::string mouseWheelUpCallback;
+		static std::string mouseWheelDownCallback;
 	};
 }
