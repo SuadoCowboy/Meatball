997dcf51ee7de46abe3c5f1c16d4ef6c11a60a90
Suado Cowboy <luccacsg@gmail.com>
2024-07-22 00:18:04 -0300

    Hello, World!

---
+++ tests/UIMaker/src/Main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+#include <raylib.h>
+
+#include <ScriptExecutor.h>
+#include <Console.h>
+#include <Input.h>
+
+int main(int argc, char** argv) {
+    if (argc < 2) {
+        std::cout << argv[0] << " <pathToScript>\n";
+        return 1;
+    }
+    
+    Meatball::Console::init([](const HayBCMD::OutputLevel&, const std::string& msg) {
+        std::cout << msg;
+    });
+    
+    lua_State* L = Meatball::Script::createLuaState();
+    if (!Meatball::Script::loadFile(L, argv[1]))
+        return 1;
+
+    SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_RESIZABLE);
+    InitWindow(1280, 600, "Meatball's Interface Creator");
+
+    while (!WindowShouldClose()) {
+        ClearBackground(BLACK);
+
+        Meatball::Input::update(false);
+
+        BeginDrawing();
+
+        EndDrawing();
+    }
+}
