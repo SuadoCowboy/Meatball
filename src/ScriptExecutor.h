7698998f63290e7e6433bc445a18bb16e5bc23a6
Suado Cowboy <luccacsg@gmail.com>
2024-07-22 15:27:21 -0300

    create vec2 macro

--- src/ScriptExecutor.h
+++ src/ScriptExecutor.h
@@ -6,6 +6,13 @@
 
 #include <HayBCMD.h>
 
+#define SCRIPT_CREATE_VEC2(L, x, y) \
+    lua_newtable(L); \
+    lua_pushnumber(L, x); \
+    lua_setfield(L, -2, "x"); \
+    lua_pushnumber(L, y); \
+    lua_setfield(L, -2, "y")
+
 namespace Meatball::Script {
     /// @brief initializes lua_State while also adding default Meatball variables
     /// @warning don't forget to use lua_close(L)!
