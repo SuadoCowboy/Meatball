c5cb66ccff7680ef8abbacab6686a58b8991cc6e
Suado Cowboy <luccacsg@gmail.com>
2024-07-22 12:31:37 -0300

    added some script functions and changed lua's print function to the Meatball::Console::print

--- src/ScriptExecutor.cpp
+++ src/ScriptExecutor.cpp
@@ -1,34 +1,99 @@
 #include "ScriptExecutor.h"
 
-#include <Input.h>
+#include <sstream>
 
-int scriptBind(lua_State* L) {
-    int nargs = lua_gettop(L);
-    if (nargs != 2)
-        return luaL_error(L, "Incorrect number of arguments. Expected 2, got %d", nargs);
+#include "Input.h"
+#include "Console.h"
 
-    if (!lua_isstring(L, 1) || !lua_isstring(L, 2))
-        return luaL_error(L, "Incorrect data type. Expected string on both parameters.");
+namespace ScriptFunctions {
+#pragma region Input
+    int bind(lua_State* L) {
+        int nargs = lua_gettop(L);
+        if (nargs != 2)
+            return luaL_error(L, "Incorrect number of arguments. Expected 2, got %d", nargs);
+
+        if (!lua_isstring(L, 1) || !lua_isstring(L, 2))
+            return luaL_error(L, "Incorrect data type. Expected string on both parameters.");
+        
+        const char* key = lua_tostring(L, 1);
+        const char* callback = lua_tostring(L, 2);
+
+        Meatball::Input::bind(key, callback);
+        return 0;
+    }
+
+    int unbind(lua_State* L) {
+        int nargs = lua_gettop(L);
+        if (nargs != 1)
+            return luaL_error(L, "Incorrect number of arguments. Expected 1, got %d", nargs);
+
+        if (!lua_isstring(L, 1))
+            return luaL_error(L, "Incorrect data type. Expected string.");
+        
+        const char* key = lua_tostring(L, 1);
+
+        Meatball::Input::unbind(key);
+        return 0;
+    }
+#pragma endregion
     
-    const char* key = lua_tostring(L, 1);
-    const char* callback = lua_tostring(L, 2);
+#pragma region Console
+    int run(lua_State* L) {
+        int nargs = lua_gettop(L);
+        if (nargs != 1)
+            return luaL_error(L, "Incorrect number of arguments. Expected 1, got %d", nargs);
 
-    Meatball::Input::bind(key, callback);
-    return 0;
+        if (!lua_isstring(L, 1))
+            return luaL_error(L, "Incorrect data type. Expected string.");
+        
+        const char* input = lua_tostring(L, 1);
+
+        Meatball::Console::run(input);
+        return 0;
+    }
+#pragma endregion
+
+    int luaPrintFunction(lua_State* L) {
+        int n = lua_gettop(L);  // Number of arguments
+        std::stringstream out;
+        for (int i = 1; i <= n; ++i) {
+            if (lua_isstring(L, i)) {
+                const char* str = lua_tostring(L, i);
+                out << str;
+            } else {
+                out << lua_typename(L, lua_type(L, i));
+            }
+            if (i < n) {
+                out << " ";  // Add a tab between arguments
+            }
+        }
+        out << "\n";  // End with a newline
+
+        Meatball::Console::printf(HayBCMD::OutputLevel::ECHO, "script: {}", out.str());
+        return 0;
+    }
 }
-
 lua_State* Meatball::Script::createLuaState() {
     lua_State* L = luaL_newstate();
     luaL_openlibs(L);
 
+    lua_pushcfunction(L, ScriptFunctions::luaPrintFunction);
+    lua_setglobal(L, "print");
+
     lua_newtable(L); // Meatball 1
     
     lua_newtable(L); // Input 2
+        lua_pushcfunction(L, ScriptFunctions::bind); // 3
+        lua_setfield(L, -2, "bind"); // pop(3)
 
-    lua_pushcfunction(L, scriptBind); // 3
-    lua_setfield(L, -2, "bind");
-
+        lua_pushcfunction(L, ScriptFunctions::unbind);
+        lua_setfield(L, -2, "unbind");
     lua_setfield(L, -2, "Input");
+
+    lua_newtable(L); // Console
+        lua_pushcfunction(L, ScriptFunctions::run);
+        lua_setfield(L, -2, "run");
+    lua_setfield(L, -2, "Console");
     
     lua_setglobal(L, "Meatball");
     
