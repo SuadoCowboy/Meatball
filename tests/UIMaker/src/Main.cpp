3529f396d048516bbf25081f161e22d1cace286e
Suado Cowboy <luccacsg@gmail.com>
2024-07-23 11:23:03 -0300

    cleaning the code a little

--- tests/UIMaker/src/Main.cpp
+++ tests/UIMaker/src/Main.cpp
@@ -17,15 +17,9 @@ struct ButtonStruct {
     int onHoverFuncRef = 0, onReleaseFuncRef = 0;
 };
 
-int main(int argc, char** argv) {
-    argc = 2;
-    if (argc < 2) {
-        std::cout << argv[0] << " <pathToScript>\n";
-        return 1;
-    }
-    
+Meatball::ConsoleUIScene* init(int width, int height) {
     SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_RESIZABLE);
-    InitWindow(800, 600, "Meatball's Interface Creator");
+    InitWindow(width, height, "Meatball's Interface Creator");
 
     Meatball::Defaults::init("data/meatdata/Init.meatdata");
     auto consoleUI = new Meatball::ConsoleUIScene(Meatball::Defaults::initLocalConsole(
@@ -33,28 +27,129 @@ int main(int argc, char** argv) {
         "data/meatdata/Console.meatdata"
     ));
 
-    consoleUI->visible = false;
-
     Meatball::Input::registerCommands();
     Meatball::Input::mapKeyboardKeys();
     Meatball::Input::mapMouseKeys();
 
+    return consoleUI;
+}
+ 
+/// @return Meatbll.UI.LayoutTypes.NONE
+unsigned char setLuaVariables(lua_State* L) {
+    unsigned char noneLayoutType;
+    { // Meatball.UI.LayoutTypes.NONE = #Meatball.UI.LayoutTypes
+        lua_getglobal(L, "Meatball");
+        lua_getfield(L, -1, "UI");
+        lua_getfield(L, -1, "LayoutTypes");
+
+        noneLayoutType = luaL_len(L, -1);
+        lua_pushinteger(L, noneLayoutType);
+        lua_setfield(L, -2, "NONE");
+
+        lua_pop(L, 3);
+    }
+
+    { // Game = {viewport = Meatball.vec2(renderWidth, renderHeight)}
+        lua_newtable(L); // Game
+            SCRIPT_CREATE_VEC2(L, GetRenderWidth(), GetRenderHeight());
+            lua_setfield(L, -2, "viewport");
+        lua_setglobal(L, "Game");
+    }
+
+    return noneLayoutType;
+}
+
+/// @brief stack idx -1 = table with button content
+/// @warning lua_State should not end while button's events are used
+/// @return new ButtonStruct()
+ButtonStruct* createButton(lua_State* L) {
+    lua_getfield(L, -1, "rect");
+        if (!Meatball::Script::isRect(L, -1))
+            luaL_error(L, "Button.rect is not a rect.");
+        
+        Rectangle rect;
+        SCRIPT_GET_RECT(L, -1, rect);
+    lua_pop(L, 1);
+
+    ButtonStruct* pButton = new ButtonStruct();
+    pButton->button = {rect};
+
+    pButton->button.config = std::make_shared<Meatball::Config::Button>(Meatball::Defaults::buttonConfig);
+    lua_getfield(L, -1, "config");
+    lua_getfield(L, -1, "color");
+        SCRIPT_GET_COLOR(L, -1, pButton->button.config->color);
+    lua_pop(L, 1);
+    lua_getfield(L, -1, "hoveredColor");
+        SCRIPT_GET_COLOR(L, -1, pButton->button.config->hoveredColor);
+    lua_pop(L, 2);
+
+    lua_getfield(L, -1, "onHover");
+    if (lua_isfunction(L, -1)) {
+        pButton->onHoverFuncRef = luaL_ref(L, LUA_REGISTRYINDEX);
+        pButton->button.onHover = [L, pButton]() {
+            lua_rawgeti(L, LUA_REGISTRYINDEX, pButton->onHoverFuncRef);
+
+            if (lua_pcall(L, 0, 0, 0) != LUA_OK) {
+                Meatball::Console::printf(HayBCMD::ERROR, "error calling onHover script: {}\n", lua_tostring(L, -1));
+                lua_pop(L, 1);
+                return;
+            }
+        };
+    } else
+        lua_pop(L, 1);
+    
+    lua_getfield(L, -1, "onRelease");
+    if (lua_isfunction(L, -1)) {
+        pButton->onReleaseFuncRef = luaL_ref(L, LUA_REGISTRYINDEX);
+        pButton->button.onRelease = [L, pButton]() {
+            lua_rawgeti(L, LUA_REGISTRYINDEX, pButton->onReleaseFuncRef);
+
+            if (lua_pcall(L, 0, 0, 0) != LUA_OK) {
+                Meatball::Console::printf(HayBCMD::ERROR, "error calling onRelease script: {}\n", lua_tostring(L, -1));
+                lua_pop(L, 1);
+                return;
+            }
+        };
+    } else
+        lua_pop(L, 1);
+
+    return pButton;
+}
+
+void handleNoneLayoutType(lua_State* L, std::vector<ButtonStruct*>& buttons) {
+    lua_pushnil(L); // push the first key
+    while (lua_next(L, 2) != 0) {
+        // FUTURE TODO: name could be used to see what is what in the window
+        //const char* name = lua_tostring(L, -2);
+
+        int objectType;
+        SCRIPT_GET_FIELD(L, -1, objectType, "type", lua_tointeger);
+
+        switch (objectType) {
+            case SCRIPT_UI_TYPE_BUTTON: {
+                ButtonStruct* pButton = createButton(L);
+                buttons.emplace_back(pButton);
+                break;
+            }
+        }
+        
+        lua_pop(L, 1);
+    }
+    lua_pop(L, 1);
+}
+
+int main(int argc, char** argv) {
+    argc = 2;
+    if (argc < 2) {
+        std::cout << argv[0] << " <pathToScript>\n";
+        return 1;
+    }
+    
+    auto consoleUI = init(800, 600);
+    consoleUI->visible = false;
+
     lua_State* L = Meatball::Script::createLuaState();
-
-    lua_getglobal(L, "Meatball");
-    lua_getfield(L, -1, "UI");
-    lua_getfield(L, -1, "LayoutTypes");
-
-    unsigned char noneType = luaL_len(L, -1);
-    lua_pushinteger(L, noneType);
-    lua_setfield(L, -2, "NONE");
-
-    lua_pop(L, 3);
-
-    lua_newtable(L); // Game
-        SCRIPT_CREATE_VEC2(L, GetRenderWidth(), GetRenderHeight());
-        lua_setfield(L, -2, "viewport");
-    lua_setglobal(L, "Game");
+    unsigned char noneLayoutType = setLuaVariables(L);
 
     if (!Meatball::Script::loadFile(L, "data/scripts/Test.lua")) {
         lua_close(L);
@@ -71,77 +166,10 @@ int main(int argc, char** argv) {
     std::vector<ButtonStruct*> buttons;
 
     int type = lua_tointeger(L, 1);
-    if (type == noneType) {
-        lua_pushnil(L); // push the first key
-        while (lua_next(L, 2) != 0) {
-            // FUTURE TODO: name could be used to see what is what in the window
-            //const char* name = lua_tostring(L, -2);
-
-            lua_getfield(L, -1, "type");
-            int objectType = lua_tointeger(L, -1);
-            lua_pop(L, 1);
-
-            switch (objectType) {
-                case SCRIPT_UI_TYPE_BUTTON: {
-                    lua_getfield(L, -1, "rect");
-                        if (!Meatball::Script::isRect(L, -1))
-                            luaL_error(L, "Button.rect is not a rect.");
-                        
-                        Rectangle rect;
-                        SCRIPT_GET_RECT(L, -1, rect);
-                    lua_pop(L, 1);
-
-                    ButtonStruct* pButton = new ButtonStruct();
-                    pButton->button = {rect};
-
-                    pButton->button.config = std::make_shared<Meatball::Config::Button>(Meatball::Defaults::buttonConfig);
-                    lua_getfield(L, -1, "config");
-                    lua_getfield(L, -1, "color");
-                        SCRIPT_GET_COLOR(L, -1, pButton->button.config->color);
-                    lua_pop(L, 1);
-                    lua_getfield(L, -1, "hoveredColor");
-                        SCRIPT_GET_COLOR(L, -1, pButton->button.config->hoveredColor);
-                    lua_pop(L, 2);
-
-                    lua_getfield(L, -1, "onHover");
-                    if (lua_isfunction(L, -1)) {
-                        pButton->onHoverFuncRef = luaL_ref(L, LUA_REGISTRYINDEX);
-                        pButton->button.onHover = [&L, pButton]() {
-                            lua_rawgeti(L, LUA_REGISTRYINDEX, pButton->onHoverFuncRef);
-
-                            if (lua_pcall(L, 0, 0, 0) != LUA_OK) {
-                                Meatball::Console::printf(HayBCMD::ERROR, "error calling onHover script: {}\n", lua_tostring(L, -1));
-                                lua_pop(L, 1);
-                                return;
-                            }
-                        };
-                    } else
-                        lua_pop(L, 1);
-                    
-                    lua_getfield(L, -1, "onRelease");
-                    if (lua_isfunction(L, -1)) {
-                        pButton->onReleaseFuncRef = luaL_ref(L, LUA_REGISTRYINDEX);
-                        pButton->button.onRelease = [&L, pButton]() {
-                            lua_rawgeti(L, LUA_REGISTRYINDEX, pButton->onReleaseFuncRef);
-
-                            if (lua_pcall(L, 0, 0, 0) != LUA_OK) {
-                                Meatball::Console::printf(HayBCMD::ERROR, "error calling onRelease script: {}\n", lua_tostring(L, -1));
-                                lua_pop(L, 1);
-                                return;
-                            }
-                        };
-                    } else
-                        lua_pop(L, 1);
-
-                    lua_pop(L, 1);
-                    buttons.emplace_back(pButton);
-                    break;
-                }
-            }
-        }
-        lua_pop(L, 1);
-    }
-    lua_pop(L, 2);
+    if (type == noneLayoutType)
+        handleNoneLayoutType(L, buttons);
+    
+    lua_pop(L, 2); // pop the UIType and UIData
 
     Vector2 viewport = {(float)GetRenderWidth(), (float)GetRenderHeight()};
 
