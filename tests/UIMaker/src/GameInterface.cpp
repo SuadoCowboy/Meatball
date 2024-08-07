894523aecd96746e02e904e7158397483d0e3a81
Suado Cowboy <luccacsg@gmail.com>
2024-08-07 10:29:41 -0300

    added updated and draw button functions

--- tests/UIMaker/src/GameInterface.cpp
+++ tests/UIMaker/src/GameInterface.cpp
@@ -2,23 +2,35 @@
 
 #include <Utils/DrawFuncs.h>
 
-void createUIObject(std::vector<UIObject*>& uiObjects, const Vector2& optionsPosition, unsigned char type, const Vector2& renderSize) {
+void updateButton(void* voidButton) {
+    Meatball::Button* button = (Meatball::Button*)voidButton;
+    button->update();
+}
+
+void drawButton(void* voidButton) {
+    Meatball::Button* button = (Meatball::Button*)voidButton;
+    Meatball::drawRect(button->rect, button->isHovered()? button->config->hoveredColor : button->config->color);
+}
+
+UIObject* createUIObject(const char* name, std::vector<UIObject*>& uiObjects, const Vector2& position, unsigned char type, const Vector2& renderSize) {
     switch (type) {
     case UI_TYPE_BUTTON: {
-        Meatball::Button* button = new Meatball::Button((Rectangle){optionsPosition.x, optionsPosition.y, renderSize.x*0.1f, renderSize.y*0.06f});
+        Meatball::Button* button = new Meatball::Button((Rectangle){position.x, position.y, renderSize.x*0.1f, renderSize.y*0.06f});
         button->config = std::make_shared<Meatball::Config::Button>(Meatball::Defaults::buttonConfig);
 
         UIObject* object = new UIObject(
                 button,
-                HayBCMD::formatString("Item{}", uiObjects.size()).c_str(),
+                name,
                 type,
-                [button](){button->update();},
-                [button](){
-                Meatball::drawRect(button->rect, button->isHovered()? button->config->hoveredColor : button->config->color);
-        });
+                updateButton,
+                drawButton
+            );
 
         uiObjects.push_back(object);
+        return object;
         break;
     }
     };
+
+    return nullptr;
 }
